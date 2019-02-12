cffc17c957cfd724875d69972317dc0a787e144b
Eddie Padin <eddie@entertainermedia.net>
2019-02-12 14:22:37 -0600

    feature: add Cards & Decks components;

---
+++ src/components/Cards/CardDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class CardDetail extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CardDetail</Text>
+      </View>
+    );
+  }
+}
+
+export default CardDetail;
---
+++ src/components/Cards/CardList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class CardList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CardList</Text>
+      </View>
+    );
+  }
+}
+
+export default CardList;
