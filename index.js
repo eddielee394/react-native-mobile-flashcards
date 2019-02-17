719aa8087e88dd14369c217210c2c865da6a1a70
Eddie Padin <eddie@entertainermedia.net>
2019-02-17 11:46:08 -0600

    fix: fixed issue with ReactToTron not loading correctly on app entry;

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { KeepAwake, registerRootComponent, Logs } from "expo";
 
 if (__DEV__) {
+  //reacttotron config
+  import("./src/config/reactotronConfig").then(() =>
+    console.log("Reactotron Configured")
+  );
+
+  //Disable expoCli logs
   const isRemoteDebuggingEnabled = typeof atob !== "undefined";
   if (isRemoteDebuggingEnabled) {
     Logs.disableExpoCliLogging();
